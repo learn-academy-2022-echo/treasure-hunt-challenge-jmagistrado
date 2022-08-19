3d6e6b6f973ebc56214187bf9d00bd62cd21da3b
jmagistrado <joycemagistrado@gmail.com>
2022-08-19 10:12:01 -0700

    Created a board

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import Square from './components/Square'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
-    }
-  }
+const App = () => {
+  
+    const [board, setBoard] = useState(["?", "?", "?", "?", "?", "?", "?", "?", "?"])
 
-  render() {
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className="boardgame">
+          {board.map((value, index) => {
+            return (
+            <Square value={value} />
+            )
+          })}
+      </div>
       </>
     )
   }
-}
+
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
-class Square extends Component {
-  render() {
+const Square = (props) => {
+
     return(
       <>
+      <div className="square">{props.value}</div>
       </>
     )
   }
-}
+
 export default Square
